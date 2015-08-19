3d7a7c0539ae02ffda48c00eb38483ae2cfcaf2d
Hristo Iliev <hsiliev@gmail.com>
2015-08-19 23:55:40 +0300

    Do not clean gems since this breaks bosh

--- scripts/cfdiego.sh
+++ scripts/cfdiego.sh
@@ -5,13 +5,10 @@ set -e -x
 STEMCELL_SOURCE=http://bosh-jenkins-artifacts.s3.amazonaws.com/bosh-stemcell/warden
 STEMCELL_FILE=latest-bosh-stemcell-warden.tgz
 
-gem cleanup
 pushd ~/workspace
   wget --progress=bar -c "${STEMCELL_SOURCE}/${STEMCELL_FILE}" -O "$STEMCELL_FILE"
   bosh -t lite -n -u admin -p admin upload stemcell --skip-if-exists $STEMCELL_FILE  
 popd
-
-gem cleanup
 
 pushd ~/workspace/diego-release
   bosh target lite
@@ -33,10 +30,8 @@ cf api --skip-ssl-validation api.10.244.0.34.xip.io
 cf auth admin admin
 cf enable-feature-flag diego_docker
 
-gem cleanup
 bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/garden-linux-release --skip-if-exists
 
-gem cleanup
 pushd ~/workspace/diego-release
   ./scripts/generate-deployment-manifest \
       ~/deployments/bosh-lite/director.yml \
@@ -50,7 +45,6 @@ pushd ~/workspace/diego-release
   bosh create release --force && bosh -t lite -n upload release && bosh -t lite -d ~/deployments/bosh-lite/diego.yml -n deploy
 popd
 
-gem cleanup
 pushd ~/workspace/diego-docker-cache-release
    ./scripts/generate-deployment-manifest ~/deployments/bosh-lite/director.yml \
        manifest-generation/bosh-lite-stubs/property-overrides.yml \
