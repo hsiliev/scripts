f06f1fe0a6459a365ecf9ffdd13e063dedfadff8
Hristo Iliev <hsiliev@gmail.com>
2015-09-10 23:48:41 +0300

    Adjust to latest diego-release changes Improve ignore file

--- scripts/cfdiego.sh
+++ scripts/cfdiego.sh
@@ -23,7 +23,7 @@ pushd ~/workspace/cf-release
   ./scripts/generate_deployment_manifest warden \
       ~/deployments/bosh-lite/director.yml \
       ~/workspace/diego-release/stubs-for-cf-release/enable_consul_with_cf.yml \
-      ~/workspace/diego-release/stubs-for-cf-release/enable_diego_ssh_in_cc.yml \
+      ~/workspace/diego-release/stubs-for-cf-release/enable_diego_ssh_in_cf.yml \
       > ~/deployments/bosh-lite/cf.yml
   bosh create release --force && bosh -t lite -n upload release && bosh -t lite -d ~/deployments/bosh-lite/cf.yml -n deploy
 popd
@@ -32,7 +32,7 @@ cf api --skip-ssl-validation api.10.244.0.34.xip.io
 cf auth admin admin
 cf enable-feature-flag diego_docker
 
-bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/garden-linux-release --skip-if-exists
+bosh upload release https://bosh.io/d/github.com/cloudfoundry-incubator/garden-linux-release --skip-if-exists || true
 
 pushd ~/workspace/diego-release
   ./scripts/generate-deployment-manifest \
