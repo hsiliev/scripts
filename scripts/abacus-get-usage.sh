6de0b22a8992c01ab92e5554feeac8c3a1a8e995
Hristo Iliev <hsiliev@gmail.com>
2016-03-02 15:32:45 +0200

    Add abacus get usage and list app usage scripts

---
+++ scripts/abacus-get-usage.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+if [ -z "$CLIENT_ID" ] || [ -z "$CLIENT_SECRET" ]; then
+  echo "Missing CLIENT_ID or CLIENT_SECRET !"
+  exit 1
+fi
+
+echo "Obtaining API endpoint URL ..."
+API=$(cf api | awk '{print $3}')
+AUTH_SERVER=${API/api./uaa.}
+echo "Using API URL $API"
+echo ""
+
+echo "Getting token for $CLIENT_ID from $AUTH_SERVER ..."
+TOKEN=$(curl --user $CLIENT_ID:$CLIENT_SECRET -s "$AUTH_SERVER/oauth/token?grant_type=client_credentials&scope=abacus.usage.linux-container.write%20abacus.usage.linux-container.read" | jq -r .access_token)
+if [ -z "$TOKEN" ]; then
+  echo "No token found !"
+  exit 1
+fi
+echo "Token obtained"
+echo ""
+
+echo "Get organization $1 guid ..."
+ORG_GUID=$(cf org $1 --guid)
+echo "Done."
+echo ""
+
+echo "Getting abacus-usage-reporting URL ..."
+USAGE_REPORTING=$(cf app abacus-usage-reporting | awk '{if (NR == 7) {print $2}}')
+echo "Using $USAGE_REPORTING"
+echo "" 
+
+echo "Getting report for organization $1 with guid $ORG_GUID ..."
+curl -sH "Authorization: bearer $TOKEN" -H "Content-Type: application/json" "https://$USAGE_REPORTING/v1/metering/organizations/${ORG_GUID}/aggregated/usage" | jq .spaces[0]
---
+++ scripts/list-app-usage-event.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+if [ -z "$CF_CLIENT_ID" ] || [ -z "$CF_CLIENT_SECRET" ]; then
+  echo "Missing CF_CLIENT_ID or CF_CLIENT_SECRET !"
+  exit 1
+fi
+
+echo "Obtaining API endpoint URL ..."
+API=$(cf api | awk '{print $3}')
+AUTH_SERVER=${API/api./uaa.}
+echo "Using API URL $API"
+echo ""
+
+echo "Getting token for $CF_CLIENT_ID from $AUTH_SERVER ..."
+TOKEN=$(curl --user "$CF_CLIENT_ID:$CF_CLIENT_SECRET" -s "$AUTH_SERVER/oauth/token?grant_type=client_credentials" | jq -r .access_token)
+if [ -z "$TOKEN" ]; then
+  echo "No token found !"
+  exit 1
+fi
+echo "Token obtained"
+echo ""
+
+if [ -z "$1" ]; then
+  echo "Get app usage events metadata ..."
+  curl -s -H "Authorization: bearer $TOKEN" -H "Content-Type: application/json" "$API/v2/app_usage_events?results-per-page=1" | jq 'del(.resources)'
+else
+  echo "Get app usage events page $1..."
+  curl -s -H "Authorization: bearer $TOKEN" -H "Content-Type: application/json" "$API/v2/app_usage_events?results-per-page=1&page=$1"
+fi
