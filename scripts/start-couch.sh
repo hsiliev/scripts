e5ab2119b9421a41b01fa7a001dbf5eb3c307acb
Hristo Iliev <hsiliev@gmail.com>
2016-12-11 23:29:18 +0200

    Add linux version of start-couch

--- scripts/start-couch.sh
+++ scripts/start-couch.sh
@@ -2,7 +2,17 @@
 
 set -e
 
-pushd /usr/local/var/lib/couchdb
-  rm -rf *
-  couchdb
-popd
+if [ "$(uname)" == "Darwin" ]; then
+  pushd /usr/local/var/lib/couchdb
+    rm -rf *
+    couchdb
+  popd
+else
+  sudo chmod 755 /var/lib/couchdb
+  pushd /var/lib/couchdb
+    sudo rm -rf ./*
+    sudo rm -rf *
+  popd
+  sudo chmod 750 /var/lib/couchdb
+  sudo service couchdb restart
+fi
