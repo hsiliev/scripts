02060c291f0f49d366b316daa522b8512b88f1ef
Hristo Iliev <hsiliev@gmail.com>
2017-09-05 10:19:09 +0300

    Improve post script

--- scripts/abacus-post-usage.sh
+++ scripts/abacus-post-usage.sh
@@ -10,6 +10,10 @@ if [ -z "$1" ]; then
   echo "No organization specified !"
   exit 1
 fi
+SCOPE="abacus.usage.read"
+if [ -n "$RESOURCE_ID" ]; then
+  SCOPE="abacus.usage.$RESOURCE_ID.write"
+fi
 
 echo "Obtaining API endpoint URL ..."
 API=$(cf api | awk '{if (NR == 1) {print $3}}')
@@ -17,8 +21,8 @@ AUTH_SERVER=${API/api./uaa.}
 echo "Using API URL $API"
 echo ""
 
-echo "Getting token for $CLIENT_ID from $AUTH_SERVER ..."
-TOKEN=$(curl --user $CLIENT_ID:$CLIENT_SECRET -s "$AUTH_SERVER/oauth/token?grant_type=client_credentials&scope=abacus.usage.iotae.write%20abacus.usage.iotae.read" | jq -r .access_token)
+echo "Getting token for $CLIENT_ID with scope $SCOPE from $AUTH_SERVER ..."
+TOKEN=$(curl --user $CLIENT_ID:$CLIENT_SECRET -s "$AUTH_SERVER/oauth/token?grant_type=client_credentials&scope=$SCOPE" | jq -r .access_token)
 if [ "$TOKEN" == "null" ] || [ -z "$TOKEN" ]; then
   echo "No token found ! Are your credentials correct (CLIENT_ID and CLIENT_SECRET)?"
   exit 1
@@ -26,21 +30,21 @@ fi
 echo "Token obtained"
 echo ""
 
-echo "Get organization $1 guid ..."
+echo "Get organization $1 and space info ..."
 set +e
 ORG_GUID=$(cf org $1 --guid)
 if [ $? != 0 ]; then
   echo "Assuming $1 is org's GUID ..."
   ORG_GUID=$1
+else
+  echo "List spaces for org $1 ..."
+  SPACE=$(cf org $1 | awk '{ if (NR==7) {print $2}}')
+  echo "Get space $SPACE guid ..."
+  SPACE_GUID=$(cf space $SPACE --guid)
+  echo "Done."
+  echo ""
 fi
 set -e
-echo "Done."
-echo ""
-
-echo "List spaces for org $1 ..."
-SPACE=$(cf org $1 | awk '{ if (NR==7) {print $2}}')
-echo "Get space $SPACE guid ..."
-SPACE_GUID=$(cf space $SPACE --guid)
 echo "Done."
 echo ""
 
@@ -62,6 +66,9 @@ echo ""
 
 DATE_IN_MS="$(date +%s000)"
 BODY="{\"start\":$DATE_IN_MS,\"end\":$DATE_IN_MS,\"organization_id\":\"$ORG_GUID\",\"space_id\":\"$SPACE_GUID\",\"resource_id\":\"iotae\",\"plan_id\":\"basic\",\"consumer_id\":\"app:1fb61c1f-2db3-4235-9934-00097845b80d\",\"resource_instance_id\":\"1fb61c1f-2db3-4235-9934-00097845b80d\",\"measured_usage\":[{\"measure\":\"warm_store\",\"quantity\":7},{\"measure\":\"cold_store\",\"quantity\":7},{\"measure\":\"aggregate_store\",\"quantity\":7},{\"measure\":\"api_calls\",\"quantity\":7}]}"
+BODY="{\"start\":$DATE_IN_MS,\"end\":$DATE_IN_MS,\"organization_id\":\"$ORG_GUID\",\"space_id\":\"73109c5e-76ae-40db-843a-7ab234abadfd\",\"consumer_id\":\"na\",\"resource_id\":\"1dc0754a-fdaf-4da7-89a1-d10124a5068c\",\"plan_id\":\"1dc0754a-fdaf-4da7-89a1-d10124a5068c-1dc0754a-fdaf-4da7-89a1-d10124a5068c\",\"resource_instance_id\":\"73109c5e-76ae-40db-843a-7ab234abadfd\",\"measured_usage\":[{\"measure\":\"api_calls\",\"quantity\":3545}]}"
+echo "curl -i -H 'Authorization: bearer $TOKEN' -H 'Content-Type: application/json' -X POST -d $BODY $URL"
+echo ""
 echo "Will submit usage $(echo $BODY | jq .)"
 echo ""
 curl -i -H "Authorization: bearer $TOKEN" -H "Content-Type: application/json" -X POST -d $BODY $URL
