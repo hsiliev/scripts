a897110bd0fb969be66a9df364a9c1f336bf71ca
Hristo Iliev <hsiliev@gmail.com>
2018-04-24 14:53:30 +0300

    Fix list app usage events script

--- scripts/list-app-usage-event.sh
+++ scripts/list-app-usage-event.sh
@@ -12,11 +12,6 @@ Shows app usage events
 EOF
 }
 
-if [ -z "$ABACUS_CF_BRIDGE_CLIENT_ID" ] || [ -z "$ABACUS_CF_BRIDGE_CLIENT_SECRET" ]; then
-  echo "Missing ABACUS_CF_BRIDGE_CLIENT_ID or ABACUS_CF_BRIDGE_CLIENT_SECRET !"
-  exit 1
-fi
-
 # A POSIX variable
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
@@ -25,7 +20,7 @@ show_all=0
 filter_org=0
 page=$1
 
-while getopts "ha:f" opt; do
+while getopts "haf:" opt; do
     case "$opt" in
       h|\?)
         show_help
@@ -33,10 +28,12 @@ while getopts "ha:f" opt; do
         ;;
       a)
         show_all=1
-        page=$OPTARG
+        unset page
         ;;
       f)
-        filter_org=1
+        show_all=1
+        unset page
+        ORG_GUID=$OPTARG
         ;;
     esac
 done
@@ -44,7 +41,7 @@ done
 shift $((OPTIND-1))
 [ "$1" = "--" ] && shift
 
-echo "Arguments: show_all='$show_all', filter_org='$filter_org', page='$page', Leftovers: $@"
+echo "Arguments: show_all='$show_all', filter_org='$ORG_GUID', page='$page', Leftovers: $@"
 echo ""
 
 echo "Obtaining API endpoint URL ..."
@@ -53,49 +50,48 @@ AUTH_SERVER=${API/api./uaa.}
 echo "Using API URL $API"
 echo ""
 
+if [ -z "$ABACUS_CF_BRIDGE_CLIENT_ID" ] || [ -z "$ABACUS_CF_BRIDGE_CLIENT_SECRET" ]; then
+  echo "Reading system user id and secret ..."
+  cf target -o SAP_abacus -s abacus
+  ABACUS_CF_BRIDGE_CLIENT_ID=$(cf env abacus-applications-bridge | grep CF_CLIENT_ID | awk '{ print $2 }')
+  ABACUS_CF_BRIDGE_CLIENT_SECRET=$(cf env abacus-applications-bridge  | grep CF_CLIENT_SECRET | awk '{ print $2 }')
+  echo ""
+fi
+
 echo "Getting token for $ABACUS_CF_BRIDGE_CLIENT_ID from $AUTH_SERVER ..."
 TOKEN=$(curl -k --user "$ABACUS_CF_BRIDGE_CLIENT_ID:$ABACUS_CF_BRIDGE_CLIENT_SECRET" -s "$AUTH_SERVER/oauth/token?grant_type=client_credentials" | jq -r .access_token)
 if [ "$TOKEN" == "null" ] || [ -z "$TOKEN" ]; then
-  echo "No token found !"
+  echo "No token found ! Are your credentials correct (ABACUS_CC_CLIENT_ID and ABACUS_CC_CLIENT_SECRET)?"
   exit 1
 fi
-echo "Token obtained"
+echo "Token $TOKEN obtained"
+echo ""
+
+echo "Reading events metadata ..."
+EVENTS=$(curl -sk -H "Authorization: bearer $TOKEN" -H "Content-Type: application/json" "$API/v2/app_usage_events?results-per-page=10000" | jq '.total_results')
+PAGES=$((EVENTS / 100 + 1))
+echo "   events: $EVENTS"
+echo "   pages : $PAGES"
 echo ""
 
 echo "App usage events metadata:"
 if [ $show_all = 1 ]; then
- curl -sk -H "Authorization: bearer $TOKEN" -H "Content-Type: application/json" "$API/v2/app_usage_events?results-per-page=10000" | jq 'del(.resources)'
+  for ((i=1;i<=PAGES;i++)); do
+    echo "Listing events on page: $i ..."
+    if [[ -z $ORG_GUID ]]; then
+      curl -sk -H "Authorization: bearer $TOKEN" -H "Content-Type: application/json" "$API/v2/app_usage_events?results-per-page=10000&order-direction=desc&page=$i" | jq .
+    else
+      curl -sk -H "Authorization: bearer $TOKEN" -H "Content-Type: application/json" "$API/v2/app_usage_events?results-per-page=10000&order-direction=desc&page=$i" | jq ".resources[] | select(.entity.org_guid == \"$ORG_GUID\")"
+    fi
+  done
 else
- curl -sk -H "Authorization: bearer $TOKEN" -H "Content-Type: application/json" "$API/v2/app_usage_events?results-per-page=1" | jq 'del(.resources)'
+  curl -sk -H "Authorization: bearer $TOKEN" -H "Content-Type: application/json" "$API/v2/app_usage_events?results-per-page=1" | jq 'del(.resources)'
+  if [ -z $page ]; then
+    echo "No page specified !"
+    exit 1
+  fi
+
+  echo "Get app usage event page #$page..."
+  curl -sk -H "Authorization: bearer $TOKEN" -H "Content-Type: application/json" "$API/v2/app_usage_events?results-per-page=10000&page=$page"
 fi
 echo ""
-
-if [ -z $page ]; then
-  echo "No page specified !"
-  exit 1
-fi
-
-if [ $show_all = 1 ]; then
-  if [ $filter_org = 1 ]; then
-    ORG=$(cf target | awk '{if (NR == 4) {print $2}}')
-    echo "Get organization $ORG guid ..."
-    set +e
-    ORG_GUID=$(cf org $ORG --guid)
-    if [ $? != 0 ]; then
-      echo "Organization $ORG not found !"
-      exit 1
-    fi
-    set -e
-    echo "Done."
-    echo ""
-
-    echo "Filtering page $page with 10000 usage events for org guid $ORG_GUID ..."
-    curl -sk -H "Authorization: bearer $TOKEN" -H "Content-Type: application/json" "$API/v2/app_usage_events?results-per-page=10000&page=$page" | jq ".resources[] | select(.entity.org_guid == \"$ORG_GUID\")"
-  else
-    echo "Listing page $page with 10000 usage events ..."
-    curl -sk -H "Authorization: bearer $TOKEN" -H "Content-Type: application/json" "$API/v2/app_usage_events?results-per-page=10000&page=$page" | jq .
-  fi
-else
-  echo "Get app usage event #$page..."
-  curl -sk -H "Authorization: bearer $TOKEN" -H "Content-Type: application/json" "$API/v2/app_usage_events?results-per-page=1&page=$page"
-fi
