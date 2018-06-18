c5073310547a09887bd6d15e4b6b95d32e7a7cb8
Hristo Iliev <hsiliev@gmail.com>
2018-06-18 16:41:54 +0300

    Delete apps with regexp

--- scripts/cf2go.sh
+++ scripts/cf2go.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ $1 == "jump" || $1 == "tunnel" || $1 == "scp" ]]; then
-  cf2go --url="https://github.infra.hana.ondemand.com/raw/cloudfoundry/sofia-landscapes/master/sof-landscapes.json" "$@" --via-host=172.18.106.18
+  cf2go "$@" --via-host=172.18.106.18
 else
-  cf2go --url="https://github.infra.hana.ondemand.com/raw/cloudfoundry/sofia-landscapes/master/sof-landscapes.json" "$@"
+  cf2go "$@"
 fi
--- scripts/delete-all-apps.sh
+++ scripts/delete-all-apps.sh
@@ -7,14 +7,16 @@ if [[ ! -d "$SCRIPT_DIR" ]]; then
   SCRIPT_DIR="$PWD";
 fi
 
-PARALLEL_JOBS=1
-if [ -n "$1" ]; then
-  PARALLEL_JOBS=$1
+if [[ -z $PARALLEL_JOBS ]]; then
+  PARALLEL_JOBS=200
 fi
 
 echo "Using $PARALLEL_JOBS parallel jobs."
 
-echo "Deleting applications ..."
-for i in {1..5}; do
-  (cf apps | tail -n +5 | awk '{print $1}' | xargs -P $PARALLEL_JOBS -n 1 $SCRIPT_DIR/delete-app.sh) && break || sleep 1;
-done
+if [ "$#" -eq 1 ]; then
+  echo "Listing $1 applications ..."
+  cf apps | tail -n +5 | awk '{print $1}' | grep -E $1 | xargs -P $PARALLEL_JOBS -n 1 $SCRIPT_DIR/delete-app.sh && break || sleep 1;
+else
+  echo "Listing $all applications ..."
+  cf apps | tail -n +5 | awk '{print $1}' | xargs -P $PARALLEL_JOBS -n 1 $SCRIPT_DIR/delete-app.sh
+fi
