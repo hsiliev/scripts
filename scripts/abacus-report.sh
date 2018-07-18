423136b4beec4335f4d7799c9c44a2c685224d30
Hristo Iliev <hsiliev@gmail.com>
2018-07-18 13:34:46 +0300

    Output reporting details on stderr

--- scripts/abacus-report.sh
+++ scripts/abacus-report.sh
@@ -10,6 +10,10 @@ Get org usage
 EOF
 }
 
+function errcho {
+  >&2 echo $1
+}
+
 # A POSIX variable
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
@@ -26,15 +30,15 @@ shift $((OPTIND-1))
 [ "$1" = "--" ] && shift
 
 if [ -z "$CLIENT_ID" ] || [ -z "$CLIENT_SECRET" ]; then
-  echo "Reading system user id and secret ..."
+  errcho "Reading system user id and secret ..."
   cf target -o SAP_abacus -s abacus
-  CLIENT_ID=$(cf env abacus-usage-collector-0 | grep CLIENT_ID | awk '{ print $2 }')
-  CLIENT_SECRET=$(cf env abacus-usage-collector-0 | grep CLIENT_SECRET | awk '{ print $2 }')
-  echo ""
+  CLIENT_ID=$(cf env abacus-usage-collector | grep CLIENT_ID | awk '{ print $2 }')
+  CLIENT_SECRET=$(cf env abacus-usage-collector | grep CLIENT_SECRET | awk '{ print $2 }')
+  errcho ""
 fi
 
 if [ -z "$1" ]; then
-  echo "No organization specified !"
+  errcho "No organization specified !"
   exit 1
 fi
 
@@ -43,44 +47,45 @@ if [ -n "$RESOURCE_ID" ]; then
   SCOPE="abacus.usage.$RESOURCE_ID.read"
 fi
 
-echo "Obtaining API endpoint URL ..."
+errcho "Obtaining API endpoint URL ..."
 API=$(cf api | awk '{if (NR == 1) {print $3}}')
 AUTH_SERVER=${API/api./uaa.}
-echo "Using API URL $API"
-echo ""
+errcho "Using API URL $API"
+errcho ""
 
-echo "Getting token for $CLIENT_ID with scope $SCOPE from $AUTH_SERVER ..."
+errcho "Getting token for $CLIENT_ID with scope $SCOPE from $AUTH_SERVER ..."
+echo ">>> curl -k --user $CLIENT_ID:$CLIENT_SECRET -X POST -s '$AUTH_SERVER/oauth/token?grant_type=client_credentials&scope=$SCOPE'"
 TOKEN=$(curl -k --user $CLIENT_ID:$CLIENT_SECRET -X POST -s "$AUTH_SERVER/oauth/token?grant_type=client_credentials&scope=$SCOPE" | jq -r .access_token)
 if [ "$TOKEN" == "null" ] || [ -z "$TOKEN" ]; then
-  echo ""
-  echo "No token found ! Running diagnostics request ..."
-  echo ">>> curl -i -k --user $CLIENT_ID:$CLIENT_SECRET -s $AUTH_SERVER/oauth/token?grant_type=client_credentials&scope=$SCOPE"
+  errcho ""
+  errcho "No token found ! Running diagnostics request ..."
+  errcho ">>> curl -i -k --user $CLIENT_ID:$CLIENT_SECRET -s $AUTH_SERVER/oauth/token?grant_type=client_credentials&scope=$SCOPE"
   curl -i -k --user $CLIENT_ID:$CLIENT_SECRET -s "$AUTH_SERVER/oauth/token?grant_type=client_credentials&scope=$SCOPE"
-  echo ""
-  echo "Are your credentials (CLIENT_ID, CLIENT_SECRET and RESOURCE_ID) correct?"
+  errcho ""
+  errcho "Are your credentials (CLIENT_ID, CLIENT_SECRET and RESOURCE_ID) correct?"
   exit 1
 fi
-echo "Obtained token"
-echo ""
+errcho "Obtained token"
+errcho ""
 
-echo "Get organization $1 guid ..."
+errcho "Get organization $1 guid ..."
 set +e
 ORG_GUID=$(cf org $1 --guid)
 if [ $? != 0 ]; then
-  echo "Assuming $1 is org's GUID ..."
+  errcho "Assuming $1 is org's GUID ..."
   ORG_GUID=$1
 fi
 set -e
-echo "Done."
-echo ""
+errcho "Done."
+errcho ""
 
-echo "Getting current domain ..."
+errcho "Getting current domain ..."
 DOMAIN=$(cf domains | awk '{if (NR == 3) {print $1}}')
 DOMAIN=${DOMAIN/cfapps/cf}
-echo "Using domain $DOMAIN"
-echo ""
+errcho "Using domain $DOMAIN"
+errcho ""
 if [ -z "$DOMAIN" ] || [ "$DOMAIN" == 'Failed' ]; then
-  echo "No domain found ! Are your logged in CF?"
+  errcho "No domain found ! Are your logged in CF?"
   exit 1
 fi
 
@@ -89,9 +94,9 @@ if [[ -z $DATE_IN_MS ]]; then
 fi
 URL="https://${ABACUS_PREFIX}abacus-usage-reporting.$DOMAIN/v1/metering/organizations/${ORG_GUID}/aggregated/usage/$DATE_IN_MS"
 
-echo "Using $URL"
-echo ""
+errcho "Using $URL"
+errcho ""
 
-echo "Getting report for org $1 ($ORG_GUID) from $URL ..."
-echo ">>> curl -k -H 'Authorization: bearer $TOKEN' -H 'Content-Type: application/json' $URL | jq ."
+errcho "Getting report for org $1 ($ORG_GUID) from $URL ..."
+errcho ">>> curl -k -H 'Authorization: bearer $TOKEN' -H 'Content-Type: application/json' $URL | jq ."
 curl -k -s -H "Authorization: bearer $TOKEN" -H "Content-Type: application/json" $URL | jq .
