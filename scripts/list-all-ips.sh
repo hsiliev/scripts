515af841ea6f664bf1c589631e9727c6969c61ec
Hristo Iliev <hsiliev@gmail.com>
2018-07-19 16:32:22 +0300

    Add list ips script

---
+++ scripts/list-all-ips.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$SCRIPT_DIR" ]]; then
+  SCRIPT_DIR="$PWD";
+fi
+
+if [[ -z $PARALLEL_JOBS ]]; then
+  PARALLEL_JOBS=200
+fi
+echo "Using $PARALLEL_JOBS parallel jobs."
+
+if [ "$#" -eq 1 ]; then
+  echo "Listing $1 applications ..."
+  cf apps | tail -n +5 | awk '{print $1}' | grep -E $1 | xargs -P $PARALLEL_JOBS -n 1 $SCRIPT_DIR/list-ip.sh
+else
+  echo "Listing $all applications ..."
+  cf apps | tail -n +5 | awk '{print $1}' | xargs -P $PARALLEL_JOBS -n 1 $SCRIPT_DIR/list-ip.sh
+fi
