8c64b7bf0911e5205afafb45054bd1faf191886a
Hristo Iliev <hsiliev@gmail.com>
2018-11-06 15:33:25 +0200

    Improve error handling in service tunnel script

--- scripts/service-tunnel.rb
+++ scripts/service-tunnel.rb
@@ -3,44 +3,48 @@
 require 'set'
 
 app = ARGV[0] || 'abacus-housekeeper'
-puts "Using application #{ENV['ABACUS_PREFIX']}#{app}"
+prefixed_app = "#{ENV['ABACUS_PREFIX']}#{app}"
+puts "Using application #{prefixed_app}"
 
-environment_variables = %x[ cf env "#{ENV['ABACUS_PREFIX']}#{app}" | grep -E 'DB_.*_URI' ].split("\n")
+environment_variables = `cf env "#{prefixed_app}" | grep -E 'DB_.*_URI'`.split("\n")
+raise "Cannot get environment for application #{prefixed_app}. Are you logged in Cloud Foundry?" unless $?.exitstatus.zero?
 
 port = 27018
 
 processed_uris = Set.new
 
-environment_variables.each { |variable|
-  name, concatenated_uris = variable.split(": ")
+def remove_trailing_and_leading(uri)
+  uri.scan(/@.*\//).first[1..-2]
+end
+
+environment_variables.each do |variable|
+  name, concatenated_uris = variable.split(': ')
   name = name[3..-5].downcase
-  uris = concatenated_uris.split("|")
+  uris = concatenated_uris.split('|')
 
   puts "   Establishing tunnel for #{name} ..."
 
-  uris.each { |uri|
-    if processed_uris.include?(uri)
-      break
-    end
+  uris.each do |uri|
+    break if processed_uris.include?(uri)
 
-    concatenated_addresses = uri.scan(/@.*\//).first[1..-2] # remove leading and trailing chars
+    concatenated_addresses = remove_trailing_and_leading(uri)
     addresses = concatenated_addresses.split(',')
 
-    addresses.each { |address|
-      Process.spawn(%Q(cf ssh -N -T -L #{port}:#{address} "#{ENV['ABACUS_PREFIX']}#{app}"))
+    addresses.each do |address|
+      Process.spawn(%(cf ssh -N -T -L #{port}:#{address} "#{ENV['ABACUS_PREFIX']}#{app}"))
       sleep 10
 
       connection_string = "localhost:#{port}"
-      master = %x[ mongo #{connection_string} --quiet --eval "d=db.isMaster(); print( d['ismaster'] );" ].strip()
+      master = `mongo #{connection_string} --quiet --eval "d=db.isMaster(); print( d['ismaster'] );"`.strip()
       port += 1
 
-      if master == 'true'
-        local_uri = uri.sub(concatenated_addresses, connection_string).split('?').first
-        puts "      #{local_uri}"
-        break
-      end
-    }
+      next unless master == 'true'
+
+      local_uri = uri.sub(concatenated_addresses, connection_string).split('?').first
+      puts "      #{local_uri}"
+      break
+    end
 
     processed_uris << uri
-  }
-}
+  end
+end
