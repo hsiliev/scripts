a7dd94499abbffbca3d9beec2911f6710ede4392
Hristo Iliev <hsiliev@gmail.com>
2019-02-21 16:21:14 +0200

    Add start flag to list-app-envs

--- scripts/list-all-app-environments.sh
+++ scripts/list-all-app-environments.sh
@@ -3,10 +3,11 @@ set -e
 
 function show_help {
   cat << EOF
-Usage: ${0##*/} [-hp:]
+Usage: ${0##*/} [-ha:p:]
 
 Shows all app environments
   -h    display this help and exit
+  -a    start from app number
   -p    search processes
 EOF
 }
@@ -20,8 +21,9 @@ OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
 # Initialize our own variables
 processes=5
+app_number=1
 
-while getopts "hp:" opt; do
+while getopts "ha:p:" opt; do
     case "$opt" in
       h|\?)
         show_help
@@ -29,6 +31,9 @@ while getopts "hp:" opt; do
         ;;
       p)
         processes=$OPTARG
+        ;;
+      a)
+        app_number=$OPTARG
         ;;
     esac
 done
@@ -39,6 +44,7 @@ shift $((OPTIND-1))
 APPS=$(cf curl "/v2/apps?results-per-page=1" | jq .total_results)
 echoerr "Apps: $APPS"
 echoerr "Processes: $processes"
+echoerr "App number: $app_number"
 echoerr ""
 
-seq 1 $APPS | xargs -P $processes -n 1 list-app-environment.sh
+seq $app_number $APPS | xargs -P $processes -n 1 list-app-environment.sh
