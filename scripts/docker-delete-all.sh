1ecd32bc2b35a8c1c9ca658b6b45eea43bf2e66c
Hristo Iliev <hsiliev@gmail.com>
2020-04-28 22:47:48 +0300

    Improve list all apps script

--- scripts/docker-delete-all.sh
+++ scripts/docker-delete-all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Delete all containers ..."
-docker rm $(docker ps -a -q)
+docker rm -fv $(docker ps -a -q)
 
 echo "Delete all images ..."
-docker rmi $(docker images -q)
+docker rmi -f $(docker images -q)
--- scripts/list-all-apps.sh
+++ scripts/list-all-apps.sh
@@ -3,12 +3,13 @@ set -e
 
 function show_help {
   cat << EOF
-Usage: ${0##*/} [-hao:] <page number>
+Usage: ${0##*/} [-ha:o:]
 
 Shows all apps
   -h    display this help and exit
   -a    display all pages
   -o    filter organization
+  -p    page number
 EOF
 }
 
@@ -20,22 +21,24 @@ function echoerr {
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
 # Initialize our own variables
-filter_org=0
-all_pages=0
-page=$1
+FILTER_ORG=0
+ALL_PAGES=0
 
-while getopts "hao:" opt; do
+while getopts "hao:p:" opt; do
     case "$opt" in
       h|\?)
         show_help
         exit 0
         ;;
       a)
-        all_pages=1
+        ALL_PAGES=1
         ;;
       o)
-        filter_org=1
+        FILTER_ORG=1
         ORG_GUID=$OPTARG
+        ;;
+      p)
+        PAGE=$OPTARG
         ;;
     esac
 done
@@ -43,7 +46,7 @@ done
 shift $((OPTIND-1))
 [ "$1" = "--" ] && shift
 
-echoerr "Arguments: show_all='$show_all', org='$ORG_GUID', page='$page', Leftovers: $@"
+echoerr "Arguments: show_all='$show_all', org='$ORG_GUID', page='$PAGE', Leftovers: $*"
 echoerr ""
 
 echoerr "Apps metadata:"
@@ -51,17 +54,18 @@ APPS=$(cf curl "/v2/apps?results-per-page=1" | jq '.total_results')
 PAGES=$(cf curl "/v2/apps?results-per-page=100" | jq '.total_pages')
 echoerr "   apps: $APPS"
 echoerr "   pages : $PAGES"
+echoerr "   pages : $PAGES"
 echoerr ""
 
-if [ $all_pages = 1 ]; then
+if [ $ALL_PAGES = 1 ]; then
   FILTER=""
 
-  if [[ $filter_org = 1 ]]; then
+  if [[ $FILTER_ORG = 1 ]]; then
     if [[ -z $ORG_GUID ]]; then
       ORG=$(cf target | awk '{if (NR == 4) {print $2}}')
       echoerr "Get organization $ORG guid ..."
       set +e
-      ORG_GUID=$(cf org $ORG --guid)
+      ORG_GUID=$(cf org "$ORG" --guid)
       if [ $? != 0 ]; then
         echoerr "Organization $ORG not found !"
         exit 1
@@ -77,16 +81,21 @@ if [ $all_pages = 1 ]; then
     FILTER="&q=organization_guid:$ORG_GUID"
   fi
 
-  for ((i=1;i<=PAGES;i++)); do
+  START_PAGE=1
+  if [ -n "$PAGE" ]; then
+    START_PAGE=$PAGE
+  fi
+
+  for ((i=START_PAGE;i<=PAGES;i++)); do
     echoerr "Listing apps on page: $i ..."
     cf curl "/v2/apps?results-per-page=100&page=$i$FILTER" | jq ".resources[].entity"
   done
 else
-  if [ -z $page ]; then
+  if [ -z "$PAGE" ]; then
     echoerr "No page specified !"
     exit 1
   fi
 
-  echoerr "Get apps #$page..."
-  cf curl "/v2/apps?results-per-page=1&page=$page"
+  echoerr "Get apps from page #$PAGE..."
+  cf curl "/v2/apps?results-per-page=1&page=$PAGE"
 fi
