f405847a0dd37a47550c8fbd6f86b3a84c59f951
Hristo Iliev <hsiliev@gmail.com>
2020-09-21 15:17:58 +0300

    Docker system prune

--- scripts/docker-delete-all.sh
+++ scripts/docker-delete-all.sh
@@ -5,3 +5,6 @@ docker rm -fv $(docker ps -a -q)
 
 echo "Delete all images ..."
 docker rmi -f $(docker images -q)
+
+echo "Delete all unused resources ..."
+docker system prune --all --force --volumes
